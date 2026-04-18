ccf9e56d983fe94a9da53d42f03013e2fade68ae
DawrAli33285 <lemightyeagle@gmail.com>
2026-04-18 22:20:59 +0500

    new

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,4 +1,4 @@
-export const BASE_URL="https://patientleakagebackend.vercel.app"
+export const BASE_URL="https://patientleakagebackend.vercel.app/api"
 
 // export const BASE_URL="http://localhost:3001/api"
 
